1f91ed4e40da7be71a709145df83beca82a29772
Mujtaba12-al <mujtaba@mativeinc.com>
2025-04-11 15:47:26 +0500

    google analytics1

--- app/Providers.js
+++ app/Providers.js
@@ -1,8 +1,28 @@
-// app/Providers.js
-"use client"; // Mark this as a Client Component
+// // app/Providers.js
+// "use client"; // Mark this as a Client Component
 
-import { SessionProvider } from "next-auth/react";
+// import { SessionProvider } from "next-auth/react";
+
+// export default function Providers({ children }) {
+//     return <SessionProvider>{children}</SessionProvider>;
+// }
+
+'use client';
+
+import { useEffect } from 'react';
+import { usePathname } from 'next/navigation';
+import { SessionProvider } from 'next-auth/react';
 
 export default function Providers({ children }) {
-    return <SessionProvider>{children}</SessionProvider>;
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (typeof window.gtag === 'function') {
+      window.gtag('config', 'G-FCK76CXTKB', {
+        page_path: pathname,
+      });
+    }
+  }, [pathname]);
+
+  return <SessionProvider>{children}</SessionProvider>;
 }
--- app/layout.js
+++ app/layout.js
@@ -27,6 +27,29 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
+<head>
+        
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-FCK76CXTKB`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-FCK76CXTKB', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
       >
